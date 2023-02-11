b461e7a6cf78d38b5b6c64f769b4bcf891152333
isha789 <isha.waheed1810@gmail.com>
2023-02-11 19:16:44 +0100

    Game-Assignment

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
-
-const choices = ["rock", "paper", "scissors"];
-
+const options = ['rock', 'paper', 'scissors'];
 let playerWins = 0;
 let computerWins = 0;
-
-const buttons = document.querySelectorAll("button");
-buttons.forEach(button => {
-  button.addEventListener("click", function() {
-    playRound(this.textContent);
-  });
-});
-
-let playerChoice = ["paper","Scissor","rock"]
-
-function playRound(playerChoice) {
-  let computerChoice = choices[Math.floor(Math.random() * 3)];
-
-  if (playerChoice === computerChoice) {
-    alert("Tie!");
-  } else if (
-    (playerChoice === "rock" && computerChoice === "scissors") ||
-    (playerChoice === "paper" && computerChoice === "rock") ||
-    (playerChoice === "scissors" && computerChoice === "paper")
-  ) {
-    playerWins++;
-    alert("You win this round!");
-  } else {
-    computerWins++;
-    alert("The computer wins this round!");
+for (let i = 0; i < 5; i++) {
+  let playerSelection = prompt("Enter rock, paper, or scissors (or press Cancel to exit):");
+  if (playerSelection === null) {
+    console.log("You have exited the game.");
+    break;
   }
-
-  if (playerWins === 5 || computerWins === 5) {
-    if (playerWins === 5) {
-      alert("You win the game!");
-    } else {
-      alert("The computer wins the game!");
+  playerSelection = playerSelection.toLowerCase();
+  while (!options.includes(playerSelection)) {
+    playerSelection = prompt("Invalid input. Please enter rock, paper, or scissors (or press Cancel to exit):");
+    if (playerSelection === null) {
+      console.log("You have exited the game.");
+      break;
     }
-    buttons.forEach(button => button.disabled = true);
+    playerSelection = playerSelection.toLowerCase();
   }
+  if (playerSelection === null) {
+    break;
+  }
+  const computerSelection = options[Math.floor(Math.random() * options.length)];
+  console.log(`Round ${i + 1}:`);
+  console.log(`Player: ${playerSelection}`);
+  console.log(`Computer: ${computerSelection}`);
+  if (playerSelection === computerSelection) {
+    console.log("It's a tie!");
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    console.log("You win this round!");
+    playerWins++;
+  } else {
+    console.log("You lose this round.");
+    computerWins++;
+  }
+  console.log(`Score: Player - ${playerWins} | Computer - ${computerWins}`);
+  console.log("\n");
 }
+if (playerWins > computerWins) {
+  console.log("You win the game!");
+} else if (playerWins < computerWins) {
+  console.log("You lose the game.");
+} else {
+  console.log("The game is a tie.");
+}
+
+
+
+
+
